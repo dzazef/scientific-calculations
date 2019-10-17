969bc5f621210c93e1cf853cba22138d29838dfe
dzazef <dzazef@gmail.com>
2019-10-18 00:32:35 +0200

    Add zad1

--- lista1/zad1.jl
+++ lista1/zad1.jl
@@ -1,9 +1,12 @@
+# Lista 1: zadanie 1
+# autor: Józef Piechaczek
+
 function macheps(type)
     i = type(1)
         prev = type(0)
     while(1 + i > 1) 
         prev = i
-        i = i / 2
+        i /= 2
     end
     prev
 end
@@ -17,7 +20,7 @@ function eta(type)
     prev = type(0)
     while(i > 0)
         prev = i
-        i = i / 2
+        i /= 2
     end
     prev
 end
@@ -26,3 +29,16 @@ println("MyEta: ", eta(Float16), " Eta: ", nextfloat(Float16(0.0)))
 println("MyEta: ", eta(Float32), " Eta: ", nextfloat(Float32(0.0)))
 println("MyEta: ", eta(Float64), " Eta: ", nextfloat(Float64(0.0)))
 
+function inf(type)
+    i = prevfloat(type(1.0))
+    prev = type(0)
+    while(!isinf(i))
+        prev = i
+        i *= 2
+    end
+    prev
+end
+
+println("MyMax: ", inf(Float16), " Max: ", floatmax(Float16))
+println("MyMax: ", inf(Float32), " Max: ", floatmax(Float32))
+println("MyMax: ", inf(Float64), " Max: ", floatmax(Float64))
